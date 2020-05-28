65a17fe2a8d77aca35dcd75157370a6a6d8ac1ba
Brandon Jones <bcjones1@fullsail.edu>
2020-05-28 03:59:06 -0400

    Working Chameleon issues

--- Build/Android/src/com/ESS/HuntGame/OBBData.java
+++ Build/Android/src/com/ESS/HuntGame/OBBData.java
@@ -19,7 +19,7 @@ public static final XAPKFile[] xAPKS = {
 new XAPKFile(
 true, // true signifies a main file
 "8", // the version of the APK that the file was uploaded against
-548705256L // the length of the file in bytes
+541642176L // the length of the file in bytes
 )
 };
 };
