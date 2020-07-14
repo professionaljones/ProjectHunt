c64a6ec963052337af8b316a7e2653953e0907ed
Brandon Jones <bcjones1@fullsail.edu>
2020-07-14 01:58:07 -0400

    Create new weapon test map for player, rebuilt lighting in Level2
    Added new timer for style speed modifier
    Updated Android version numbers

--- Build/Android/src/com/ESS/HuntGame/OBBData.java
+++ Build/Android/src/com/ESS/HuntGame/OBBData.java
@@ -18,8 +18,8 @@ mFileSize = fileSize;
 public static final XAPKFile[] xAPKS = {
 new XAPKFile(
 true, // true signifies a main file
-"9", // the version of the APK that the file was uploaded against
-589852026L // the length of the file in bytes
+"3", // the version of the APK that the file was uploaded against
+658405348L // the length of the file in bytes
 )
 };
 };
