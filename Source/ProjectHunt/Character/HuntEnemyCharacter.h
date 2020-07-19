2c21bf61641e2f84c42efada526257a72191d302
Brandon Jones <bcjones1@fullsail.edu>
2020-07-19 04:35:34 -0400

    Refactor and clean up Code, blueprints

---
+++ Source/ProjectHunt/Character/HuntEnemyCharacter.h
@@ -0,0 +1,17 @@
+// Copyright 2020 Brandon Jones. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ProjectHuntCharacter.h"
+#include "HuntEnemyCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTHUNT_API AHuntEnemyCharacter : public AProjectHuntCharacter
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/ProjectHunt/Character/Player/PlayerStatsComponent.h
@@ -0,0 +1,17 @@
+// Copyright 2020 Brandon Jones. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/HuntStatsComponent.h"
+#include "PlayerStatsComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTHUNT_API UPlayerStatsComponent : public UHuntStatsComponent
+{
+	GENERATED_BODY()
+	
+};
